85b06d4751b450ca409fd63b76e2378ffba091ba
André Luiz Morita <andreluizmorita@gmail.com>
2019-05-18 18:15:23 -0300

    Page Welcome

--- src/pages/Welcome/index.js
+++ src/pages/Welcome/index.js
@@ -1,12 +1,31 @@
 import React from 'react';
 
-import { View } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity, StatusBar } from 'react-native';
 
 import styles from './styles';
 
 const Welcome = () => (
   <View style={styles.container}>
-    <Text>Testando</Text>
+    <StatusBar barStyle="light-content" />
+    <Text style={styles.title}>Bem-vindo</Text>
+    <Text style={styles.text}>
+      Para continuar precisamos que você informe seu usuário no GitHub.
+    </Text>
+    <View style={styles.form}>
+      <TextInput
+        style={styles.input}
+        autoCapitalizae="none"
+        autoCorrect={false}
+        placeholder="Digite seu usuário"
+        underlineColorAndroid="transparent"
+      />
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => {}}
+      >
+        <Text style={styles.buttonText}>Prossegir</Text>
+      </TouchableOpacity>
+    </View>
   </View>
 );
 
