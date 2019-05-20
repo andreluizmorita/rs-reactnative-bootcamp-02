9f2c3eba98aaa644dff57382f5e99cc1d93c3d87
André Luiz Morita <andreluizmorita@gmail.com>
2019-05-20 15:09:29 -0300

    List and refresh

--- src/pages/Repositories/index.js
+++ src/pages/Repositories/index.js
@@ -1,16 +1,63 @@
-import React from 'react';
-import { View, Text, AsyncStorage } from 'react-native';
+import React, { Component } from 'react';
+import {
+  View, Text, AsyncStorage, ActivityIndicator, FlatList,
+} from 'react-native';
 import PropTypes from 'prop-types';
 
 import Icon from 'react-native-vector-icons/FontAwesome';
 
 import Header from '~/components/Header';
+import RepositoryItem from './RespositoryItem';
+import api from '~/services/api';
+import styles from './styles';
 
-const Repositories = () => (
-  <View>
-    <Header title="Repositories" />
-  </View>
-);
+class Repositories extends Component {
+  state = {
+    data: [],
+    loading: true,
+    refreshing: false,
+  };
+
+  componentDidMount() {
+    this.loadRepositories();
+  }
+
+  loadRepositories = async () => {
+    this.setState({ refreshing: true });
+
+    const username = await AsyncStorage.getItem('@GitHuber:username');
+    const { data } = await api.get(`/users/${username}/repos`);
+
+    this.setState({ data, loading: false, refreshing: false });
+  };
+
+  renderListItem = ({ item }) => <RepositoryItem repository={item} />;
+
+  renderList = () => {
+    const { data, refreshing } = this.state;
+
+    return (
+      <FlatList
+        data={data}
+        keyExtractor={item => String(item.id)}
+        renderItem={this.renderListItem}
+        onRefresh={this.loadRepositories}
+        refreshing={refreshing}
+      />
+    );
+  };
+
+  render() {
+    const { loading } = this.state;
+
+    return (
+      <View style={styles.container}>
+        <Header title="Repositories" />
+        {loading ? <ActivityIndicator style={styles.loading} /> : this.renderList()}
+      </View>
+    );
+  }
+}
 
 const TabIcon = ({ tintColor }) => <Icon name="list-alt" size={20} color={tintColor} />;
 
